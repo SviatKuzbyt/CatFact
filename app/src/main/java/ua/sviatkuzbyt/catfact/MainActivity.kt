b1511168f371f0f47ca6c870c659b2bef1f67a59
Sviatoslav Kuzbyt <sviatoslav.kuzbyt@gmail.com>
2025-07-24 15:49:49 +0300

    add dependencies

---
+++ app/src/main/java/ua/sviatkuzbyt/catfact/MainActivity.kt
@@ -0,0 +1,24 @@
+package ua.sviatkuzbyt.catfact
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Text(
+                text = "Hello CatFact!",
+                modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars)
+            )
+        }
+    }
+}
